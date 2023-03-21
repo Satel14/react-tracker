c959de84b9bb89a8fe49e37e6fd9e4e1784a8fec
Satel14 <ostaplvov@gmail.com>
2023-03-21 17:51:21 +0200

    feat: add function to store last searched players' data in a JSON file

--- backend/services/player.js
+++ backend/services/player.js
@@ -1,9 +1,38 @@
 const MESSAGE = require("../constant/responeMessages");
 
-module.exports.getPlayerRank = async () =>{
-    try{
-        return [];
-    }catch (e){
-        throw Error(e.message);
-    }
+module.exports.getPlayerRank = async () => {
+  try {
+    return [];
+  } catch (e) {
+    throw Error(e.message);
+  }
 };
+
+module.exports.addLastSearchers = async (data) => {
+  try {
+    const fs = require("fs")
+    const fileName = '../json/last-searcheds.json';
+    const file = require(fileName)
+
+    if (file[data.gameid]) {
+      delete file[data.gameid]
+    }
+
+    file[data.gameid] = data;
+
+    const length = Object.keys(file.length)
+
+    if (length > 5) {
+      delete file[Object.keys(file)[0]]
+    }
+
+    const path = require("path")
+    const finalPath = path.join("../backend/src/json", "last-searcheds.json")
+    fs.writeFileSync(finalPath, JSON.stringify(file), function writeJSON(err) {
+      if (err) return console.log(err)
+    })
+    return data
+  } catch (e) {
+    throw Error(e.message)
+  }
+}
