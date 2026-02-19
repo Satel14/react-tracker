aba3061c7c20972b8b1bee5f4411dc6208eb5a1b
Satel14 <ostaplvov@gmail.com>
2026-02-19 13:53:18 +0200

    Fix ENETUNREACH: force IPv4 for nodemailer

--- backend/controllers/email.js
+++ backend/controllers/email.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    family: 4, // Force IPv4
 });
 
 module.exports.sendBugReport = async (req, res) => {
