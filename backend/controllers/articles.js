65c7d26fde4a0139704c8a39788e28186c6496d3
Satel14 <ostaplvov@gmail.com>
2026-07-30 18:35:29 +0300

    chore: drop the unconsumed aboutme and roadmap endpoints

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -1,36 +1,4 @@
-const {
-  aboutMe,
-  bugReportText,
-  bugReportList,
-  roadmapText,
-  roadMapList,
-  socialList,
-} = require('../constant/articles')
-
-module.exports.getAboutMe = async (req, res) => {
-  try {
-    const data = {
-      aboutMe,
-      socialList,
-    }
-    return res.status(200).json({ status: 200, data })
-  } catch (e) {
-    return res.status(200).json({ statu: 200, message: e.message })
-  }
-}
-
-module.exports.getRoadmap = async (req, res) => {
-  try {
-    const data = {
-      roadmapText,
-      roadMapList,
-    }
-    return res.status(200).json({ status: 200, data })
-  } catch (e) {
-    return res.status(200).json({ statu: 200, message: e.message })
-  }
-}
-
+const { bugReportText, bugReportList } = require('../constant/articles')
 
 module.exports.getBugReport = async (req, res) => {
   try {
